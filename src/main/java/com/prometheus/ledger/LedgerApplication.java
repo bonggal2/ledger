d375fbe40a85399f56fca632c8766784fc43c610
Bonggal Siahaan <bonggal.siahaan@gmail.com>
2020-06-19 16:34:46 +0700

    Add configuration

--- src/main/java/com/prometheus/ledger/LedgerApplication.java
+++ src/main/java/com/prometheus/ledger/LedgerApplication.java
@@ -2,8 +2,10 @@ package com.prometheus.ledger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({"beans.xml"})
 public class LedgerApplication {
 
 	public static void main(String[] args) {
