e0e3f805069f34d20f146844f7b144248c9ffa05
Bonggal Siahaan <bonggal.siahaan@gmail.com>
2020-06-21 16:40:02 +0700

    update login logic

--- src/main/java/com/prometheus/ledger/controller/ledger/LedgerController.java
+++ src/main/java/com/prometheus/ledger/controller/ledger/LedgerController.java
@@ -1,5 +1,8 @@
 package com.prometheus.ledger.controller.ledger;
 
+import com.prometheus.ledger.core.util.StringUtil;
+import com.prometheus.ledger.service.common.session.SessionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +15,16 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping(value = "/ledger")
 public class LedgerController {
 
+    @Autowired
+    private SessionService sessionService;
+
     private static final String TOTAL_EXPENSES = "totalExpenses";
 
     @RequestMapping(value = {"/home"}, method = RequestMethod.GET)
     public String ledgerMainPage(HttpServletRequest request, HttpServletResponse response, Model model){
+        if(StringUtil.isBlank(sessionService.getLoginSession(request.getSession()).getUserId())){
+            return "redirect:/login";
+        }
 
         // getTotalExpenses
         // getTotalIncomes
