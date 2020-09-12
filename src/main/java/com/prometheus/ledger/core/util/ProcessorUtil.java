064f04685db725fd3e5cc716a4f02191f3406c9d
bonggal.siahaan <bonggal.siahaan@dana.id>
2020-09-13 01:16:49 +0700

    break if process failed

--- src/main/java/com/prometheus/ledger/core/util/ProcessorUtil.java
+++ src/main/java/com/prometheus/ledger/core/util/ProcessorUtil.java
@@ -1,23 +1,31 @@
 package com.prometheus.ledger.core.util;
 
 import com.prometheus.ledger.core.model.BaseProcessContext;
-import com.prometheus.ledger.core.model.Money;
 import com.prometheus.ledger.core.model.Processor;
+import com.prometheus.ledger.core.model.result.BaseResult;
 
 import java.util.List;
 
 public class ProcessorUtil {
-    public static void runProcessors(List<Processor<BaseProcessContext>> processorList, BaseProcessContext context){
-        if (CollectionUtil.isEmpty(processorList)){
+    public static void runProcessors(List<Processor<BaseProcessContext>> processorList, BaseProcessContext context) {
+        if (CollectionUtil.isEmpty(processorList)) {
             return;
         }
 
-        processorList.forEach(processor -> {
-            if (processor.isSkipped(context)) {
-                return;
-            }
-            processor.check(context);
-            processor.doProcess(context);
-        });
+        BaseResult processResult = new BaseResult();
+        processResult.setSuccess(true);
+        processorList
+                .stream().filter(processor -> processResult.isSuccess())
+                .forEach(processor -> {
+                    try {
+                        if (processor.isSkipped(context)) {
+                            return;
+                        }
+                        processor.check(context);
+                        processor.doProcess(context);
+                    } catch (Throwable e) {
+                        processResult.setSuccess(false);
+                    }
+                });
     }
 }
